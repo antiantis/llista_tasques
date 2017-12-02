9cbc402c20fe00f494902e9ecd0b3b00e467f41a
Santi Sabate <ssabate@iesebre.com>
2017-12-02 20:48:58 +0100

    Primer commit

---
+++ models/Tasca.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+
+
+class Tasca(models.Model):
+    _name = 'todo.task';
+    name = fields.Char('Descripció:',required=True)
+    isDone = fields.Boolean('Feta?')
+    isActive = fields.Boolean('Activa?', default=True)
+
+    @api.one
+    def do_toggle_done(self):
+        self.isDone = not self.isDone
+        return True
+
+    @api.multi
+    def do_clear_done(self):
+        done_recs = self.search([('isDone', '=', True)])
+        done_recs.write({'isActive': False})
+        return True
